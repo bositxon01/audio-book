89a1b80dc06bc77fb0e5f265a290d30a354698e4
Bositxon <bositxonjava@gmail.com>
2025-03-03 08:55:07 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/audiobook/AudioBookApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.audiobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudioBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/audiobook/AudioBookApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.audiobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
