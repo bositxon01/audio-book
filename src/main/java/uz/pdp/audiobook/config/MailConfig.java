e9c97bc062a4d6628d394d2b70974a4d951dc416
Zaripov <ergashaliz7008@gmail.com>
2025-03-04 16:53:31 +0500

    dsfvdsfv

---
+++ src/main/java/uz/pdp/audiobook/config/MailConfig.java
@@ -0,0 +1,15 @@
+package uz.pdp.audiobook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender mailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
