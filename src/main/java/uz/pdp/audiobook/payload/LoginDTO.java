466c3a107f8bc0d60ef6fb0dd5e30a32be8580c3
Zaripov <ergashaliz7008@gmail.com>
2025-03-07 12:53:49 +0500

    dfhnbfgnh

--- src/main/java/uz/pdp/audiobook/payload/LoginDTO.java
+++ src/main/java/uz/pdp/audiobook/payload/LoginDTO.java
@@ -1,5 +1,8 @@
 package uz.pdp.audiobook.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @AllArgsConstructor
@@ -9,8 +12,15 @@ import lombok.*;
 @ToString
 public class LoginDTO {
 
-    private String username;
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Invalid email format")
+    private String username;   // You can rename this to 'email' if preferred
 
+    @NotBlank(message = "Password must not be blank")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$",
+            message = "Password must be at least 8 characters long, contain at least one uppercase letter, one digit, and one special character (@#$%^&+=!)"
+    )
     private String password;
 
 }
--- src/main/java/uz/pdp/audiobook/payload/RegisterDTO.java
+++ src/main/java/uz/pdp/audiobook/payload/RegisterDTO.java
@@ -1,5 +1,6 @@
 package uz.pdp.audiobook.payload;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.sql.Date;
@@ -11,14 +12,27 @@ import java.sql.Date;
 @ToString
 public class RegisterDTO {
 
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Invalid email format")
     private String username;
 
+    @NotBlank(message = "Password must not be blank")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$",
+            message = "Password must be at least 8 characters long, contain at least one uppercase letter, one digit, and one special character (@#$%^&+=!)"
+    )
     private String password;
 
+    @NotBlank(message = "First name must not be blank")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     private String firstName;
 
+    @NotBlank(message = "Last name must not be blank")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
     private String lastName;
 
+    @NotNull(message = "Date of birth must not be null")
+    @Past(message = "Date of birth must be in the past")
     private Date dateOfBirth;
 
 }
