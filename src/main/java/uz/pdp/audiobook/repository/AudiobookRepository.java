62f8707af6722e1cfb447ae094e10c0027553aa3
Bositxon <bositxonjava@gmail.com>
2025-03-24 14:36:21 +0500

    some changes

--- src/main/java/uz/pdp/audiobook/repository/AudiobookRepository.java
+++ src/main/java/uz/pdp/audiobook/repository/AudiobookRepository.java
@@ -4,13 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uz.pdp.audiobook.entity.Audiobook;
 
-import java.util.List;
-import java.util.Optional;
 @Repository
 public interface AudiobookRepository extends JpaRepository<Audiobook, Integer> {
-
-    Optional<Audiobook> findByIdAndDeletedFalse(Integer id);
-
-    List<Audiobook> findByDeletedFalse();
 
 }
