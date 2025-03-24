6b902319f7a6faca0fc107a320a04f3fb295cd5f
User <ergashaliz7008@gmail.com>
2025-03-24 14:29:25 +0500

    some changes (RewievDTO)

--- src/main/java/uz/pdp/audiobook/payload/ReviewDTO.java
+++ src/main/java/uz/pdp/audiobook/payload/ReviewDTO.java
@@ -2,6 +2,7 @@ package uz.pdp.audiobook.payload;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,18 +16,14 @@ public class ReviewDTO {
     @NotNull
     private Integer userId;
 
-    private Integer id;
-
-
     @NotNull
     private Integer audioBookId;
 
-    @NotNull
+    @NotBlank
     private String content;
 
     @NotNull
     @Min(1)
     @Max(5)
     private Integer rating;
-
 }
