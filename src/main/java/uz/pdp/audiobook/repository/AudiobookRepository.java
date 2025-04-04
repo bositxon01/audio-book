055438c416e3cfd3edf3e7659be71cf70bff5ff6
User <ergashaliz7008@gmail.com>
2025-04-04 19:26:24 +0500

    gbdb

--- src/main/java/uz/pdp/audiobook/repository/AudiobookRepository.java
+++ src/main/java/uz/pdp/audiobook/repository/AudiobookRepository.java
@@ -1,9 +1,14 @@
 package uz.pdp.audiobook.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import uz.pdp.audiobook.entity.Audiobook;
 
-@Repository
-public interface AudiobookRepository extends JpaRepository<Audiobook, Integer> {
+import java.util.List;
+import java.util.Optional;
+
+public interface AudioBookRepository {
+
+    Optional<Audiobook> findByIdAndDeletedFalse(Integer id);
+
+    List<Audiobook> findByDeletedFalse(Integer id);
+
 }
